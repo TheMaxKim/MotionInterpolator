7d3efa38d0c33296cf6018c24125c247f72706b8
Max Kim <maxjukim@gmail.com>
2015-04-20 18:53:04 -0400

    Added ability to open a video file

--- MotionInterpolator/Form1.Designer.cs
+++ MotionInterpolator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MotionInterpolator
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.formButtonTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.formButtonTableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // formButtonTableLayoutPanel
+            // 
+            this.formButtonTableLayoutPanel.ColumnCount = 2;
+            this.formButtonTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.formButtonTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.formButtonTableLayoutPanel.Controls.Add(this.openFileButton, 1, 1);
+            this.formButtonTableLayoutPanel.Location = new System.Drawing.Point(61, 442);
+            this.formButtonTableLayoutPanel.Name = "formButtonTableLayoutPanel";
+            this.formButtonTableLayoutPanel.RowCount = 2;
+            this.formButtonTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.formButtonTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.formButtonTableLayoutPanel.Size = new System.Drawing.Size(467, 100);
+            this.formButtonTableLayoutPanel.TabIndex = 0;
+            this.formButtonTableLayoutPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(236, 53);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Select Video";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(581, 554);
+            this.Controls.Add(this.formButtonTableLayoutPanel);
+            this.Name = "MainForm";
+            this.Text = "Video Motion Interpolator";
+            this.formButtonTableLayoutPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel formButtonTableLayoutPanel;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
     }
 }
-
--- MotionInterpolator/Form1.cs
+++ MotionInterpolator/Form1.cs
@@ -10,11 +10,47 @@ using System.Windows.Forms;
 
 namespace MotionInterpolator
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog videoOpenFileDialog = new OpenFileDialog();
+
+            videoOpenFileDialog.Filter = "Video Files|*.avi;*.mp4";
+            videoOpenFileDialog.FilterIndex = 1;
+
+            videoOpenFileDialog.Multiselect = false;
+
+            
+
+            if (videoOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    System.IO.Stream fileStream;
+
+                    if ((fileStream = videoOpenFileDialog.OpenFile()) != null)
+                    {
+                        MessageBox.Show("File Selected", "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + exception.Message);
+                }
+            }
+
+
         }
     }
 }
